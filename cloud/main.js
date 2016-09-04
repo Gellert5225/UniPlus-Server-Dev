3f8b19740094f94c7e34ad046ea289fea06f7452
Gellert <gellert.li1995@gmail.com>
2016-09-04 22:17:27 +0800

    test4

--- cloud/main.js
+++ cloud/main.js
@@ -80,3 +80,28 @@ Parse.Cloud.define("commentPost", function(request, response){
 
         response.success('success');
 });
+
+//follow action
+Parse.Cloud.define("followUser", function(request, response){
+    var author  = request.params.author;
+    var message = request.params.message;
+    
+    var query = new Parse.Query(Parse.User);
+    query.equalTo('username', author);
+    
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.matchesQuery("user", query);
+    
+    Parse.Push.send({
+        where: pushQuery,
+        data: {
+            alert: message
+        }
+    },{ success: function() {
+            console.log("#### PUSH OK");
+        }, error: function(error) {
+            console.log("#### PUSH ERROR" + error.message);
+        }, useMasterKey: true});
+
+        response.success('success');
+});
