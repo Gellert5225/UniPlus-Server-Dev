35cc92641e31b6b669cc6b6882d99ab21c56d441
Gellert <gellert.li1995@gmail.com>
2016-09-04 22:11:05 +0800

    test3

--- cloud/main.js
+++ cloud/main.js
@@ -54,4 +54,29 @@ Parse.Cloud.define("likePost", function(request, response){
         }, useMasterKey: true});
 
         response.success('success');
-    });
+});
+
+//comment action
+Parse.Cloud.define("commentPost", function(request, response){
+    var author  = request.params.author;
+    var message = request.params.message;
+    
+    var query = new Parse.Query(Parse.User);
+    query.equalTo('username', author);
+    
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.matchesQuery("user", query);
+    
+    Parse.Push.send({
+        where: pushQuery,
+        data: {
+            alert: message
+        }
+    },{ success: function() {
+            console.log("#### PUSH OK");
+        }, error: function(error) {
+            console.log("#### PUSH ERROR" + error.message);
+        }, useMasterKey: true});
+
+        response.success('success');
+});
