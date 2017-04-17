6ed34a8f6471745cda43f8b8a6c4369a0d978653
Gellert <gellert.li1995@gmail.com>
2017-04-17 11:59:22 -0700

    nothing

--- index.js
+++ index.js
@@ -22,21 +22,18 @@ var api = new ParseServer({
   //       classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   //   },
   push        : {
-                ios: [
-                  {
+                ios: [{
                     pfx        : './Cert/UniPlusPush.p12', // Dev PFX or P12
                     bundleId   : 'com.JiaheLi.UniPlus',
                     production : false
                     //adaptor    : ParsePushAdapter
-                  },
-                  {
+                  },{
                     pfx        : './Cert/UniPlusPushProduction.p12', // Prod PFX or P12
                     bundleId   : 'com.JiaheLi.UniPlus',  
                     production : true // Prod
                     //adaptor    : ParsePushAdapter
-                  }
-                ]
-    }
+                  }]
+                }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
