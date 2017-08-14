bba3b6c5f7f9ae00f49a964b7f16f565ec6abd85
Gellert <gellert.li1995@gmail.com>
2017-08-13 21:46:00 -0700

    initial setup for website

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
 var express     = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path        = require('path');
@@ -39,27 +36,33 @@ var api = new ParseServer({
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
+Parse.initialize("Zj9b976AHw3b7SkOgCKzaEjB7bjJoPh2XcyxTcgU");
+Parse.serverURL = process.env.SERVER_URL || 'http://localhost:1337/parse';
+
 var app = express();
 
-// Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
-// Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
+app.use(cookieSession({
+  name: 'form-cookie',
+  secret: 'form-key',
+  maxAge: 15724800000
+}));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
 app.get('/', function(req, res) {
-  res.status(200).send('Make sure to star the parse-server repo on GitHub!');
+  res.status(200).send('Hello world');
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
 app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-//npm run start
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
