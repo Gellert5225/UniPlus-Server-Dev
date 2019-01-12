c155441b7ee292e7602204b867bfad59e370a00f
Gellert <gellert.li1995@gmail.com>
2019-01-12 21:50:52 +0800

    display comments

--- routes/question.js
+++ routes/question.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var answerSchema = {
   answer: Parse.Object,
   currentUserVote: String,
+  comments: []
 };
 
 router.get("/questions", function(req,res){ // display all questions
@@ -23,53 +24,63 @@ router.get("/questions/:id", async function(req,res){
   query.include('user');
   query.get(req.params.id)
   .then((question) => {
-    var answerRelation = question.relation("answers");
-    var answerQuery = answerRelation.query();
-    answerQuery.include("author");
-    answerQuery.descending("createdAt");
-    answerQuery.find().then(async(answers) => {
+    var questionCommentsRelation = question.relation('comments');
+    var questionCommentsQuery = questionCommentsRelation.query();
+    questionCommentsQuery.find().then(async(comments) => {
+      var answerRelation = question.relation("answers");
+      var answerQuery = answerRelation.query();
+      answerQuery.include("author");
+      answerQuery.descending("createdAt");
+      answerQuery.find().then(async(answers) => {
         var relation = question.relation("votes");
         var query = relation.query();
         query.equalTo('fromUser', Parse.User.current());
         query.equalTo('toQuestion', question);
         query.find().then(async(results) => {
-            await asyncForEach(answers, async (answer) => {
-              console.log(answer);
-              var ansVoteRelation = answer.relation('votes');
-              var query = ansVoteRelation.query();
-              query.equalTo('fromUser', Parse.User.current());
-              const answerVotes = await query.find();
-              if (answerVotes.length) {
-                console.log('found a vote');
-                answerSchemas.push({
-                  answer: answer,
-                  currentUserVote: answerVotes[0].get('type')
-                });
-              } else {
-                answerSchemas.push({
-                  answer: answer,
-                  currentUserVote: 'none'
-                });
-              }
-            });
-            if (results.length) {
-              if (results[0].get('type') == 'upVote') {
-                res.status(200).render('question', {question: question, answers: answerSchemas, currentUserVoteStat: 'upVote'});
-              } else {
-                res.status(200).render('question', {question: question, answers: answerSchemas, currentUserVoteStat: 'downVote'});
-              }
+          await asyncForEach(answers, async (answer) => {
+            var ansVoteRelation = answer.relation('votes');
+            var ansCommentRelation = answer.relation('comments');
+
+            var query = ansVoteRelation.query();
+            var ansCommentQuery = ansCommentRelation.query();
+
+            query.equalTo('fromUser', Parse.User.current());
+
+            const answerVotes = await query.find();
+            const answerComments = await ansCommentQuery.find();
+
+            var ansVote;
+            if (answerVotes.length) {
+              ansVote = answerVotes[0].get('type');
             } else {
-                res.status(200).render('question', {question: question, answers: answers, currentUserVoteStat: 'none'});
+              ansVote = 'none';
             }
-          },
-          (error) => {
-            console.log('Error: ' + error.code + ' ' + error.message);
+            answerSchemas.push({
+              answer: answer,
+              currentUserVote: ansVote,
+              comments: answerComments
+            });
+          });
+          var voteStat;
+          if (results.length) {
+            if (results[0].get('type') == 'upVote') {
+              voteStat = 'upVote';
+            } else {
+              voteStat = 'downVote';
+            }
+          } else {
+            voteStat = 'none';
           }
-        );
+          res.status(200).render('question', {question: question, questionComments: comments, answers: answerSchemas, currentUserVoteStat: voteStat});
+        },(error) => {
+            console.log('Error: ' + error.code + ' ' + error.message);
+        });
       }, (error) => {
         console.log(error);
-      }
-    );
+      });
+    }, (error) => {
+      console.log(error);
+    });
   }, (error) => {
     console.log(error);
   });
